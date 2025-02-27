e67e78dfc9fe365d1b1947ed14ab2cda5a06630a
Flávio Moura <62734053+Horusprg@users.noreply.github.com>
2025-02-27 14:37:47 -0300

    Update index.py

--- service/api/routes/index.py
+++ service/api/routes/index.py
@@ -6,5 +6,4 @@ index_router = APIRouter()
 # Redireciona a rota raiz (/) para a documentação Swagger (/docs)
 @index_router.get("/", include_in_schema=False)
 async def root(request: Request):
-    base_url = request.base_url._url.rstrip("/")  # Garante que a URL base seja obtida corretamente
-    return RedirectResponse(url=f"{base_url}/docs")
+    return RedirectResponse(url=request.scope.get("root_path", "") + "/docs")
